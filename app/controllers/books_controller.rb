3e31683e63d3e450b200b5f2d3c1db90b931d2b2
HikaruKaneko <127648417+kaneko1001@users.noreply.github.com>
2023-04-27 09:00:24 +0000

    [応用3]いいね機能実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @user = @book.user
-    @books = Book.new
+    @book_ = Book.new
   end
 
   def index
